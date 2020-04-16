a4e69e980daaa32b20e5a34d0182e8ab000dc805
Pridestalker <6103741+Pridestalker@users.noreply.github.com>
2020-04-16 11:44:39 +0200

    Added hook to different action to be more reliable

--- vmw-wc-order.php
+++ vmw-wc-order.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Vmw Wc Order
 Description: This plugin adds an after order hook to send orders to the VMW Portal
-Version: 1.0
+Version: 2.0
 Author: Mitch Hijlkema
 License: GPL2
 */
@@ -13,9 +13,26 @@ defined('ABSPATH') || exit;
 define('VMWCO_FILE', __FILE__);
 define('VMWCO_VERSION', '1.0.0');
 
-add_action('woocommerce_thankyou', function ($order_id) {
-    include_once plugin_dir_path(VMWCO_FILE) . 'vendor/autoload.php';
+/**
+ * Hooks into WooCommerce action to send a request to a Webhook.
+ *
+ * This function hooks in to `woocommerce_order_status_processing` action to
+ * send a request to the vendor portal web hook to start the order cycle
+ * Used to be on `woocommerce_thankyou` but that only fires when the
+ * order thank you page is called. Which is not 100% guaranteed.
+ *
+ * @see woocommerce_order_status_processing
+ * @param int $order_id The woocommerce order id
+ *
+ * @returns void
+ */
+add_action('woocommerce_order_status_processing', static function ($order_id) {
+	include_once plugin_dir_path(VMWCO_FILE) . 'vendor/autoload.php';
 
-    $client = Symfony\Component\HttpClient\HttpClient::create();
-    $client->request('GET', 'https://verkoper.vindmijnwijn.nl/webhooks/add-order/' . $order_id);
+	try {
+		$client = Symfony\Component\HttpClient\HttpClient::create();
+		$client->request('GET', 'https://verkoper.vindmijnwijn.nl/webhooks/add-order/' . $order_id);
+	} catch (\Exception $e) {
+		// Do nothing to interrupt order.
+	}
 }, 10, 1);
